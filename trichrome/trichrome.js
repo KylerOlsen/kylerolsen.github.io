6472e5ab9478a41d1a206017bcf422a07d1d71a7
Kyler Olsen <kylerolsen17@gmail.com>
2026-03-12 11:57:15 -0600

    Added trichrome mirror

---
+++ trichrome/trichrome.js
@@ -0,0 +1,26 @@
+// Kyler Olsen
+// Feb 2026
+
+function dark_mode() {
+    if (document.querySelector("body").getAttribute("mode") == "dark") {
+        document.querySelector("body").setAttribute("mode", "light");
+        localStorage.setItem("purple_cello_trichrome_color_mode", "light");
+    } else {
+        document.querySelector("body").setAttribute("mode", "dark");
+        localStorage.setItem("purple_cello_trichrome_color_mode", "dark");
+    }
+}
+
+function main() {
+    document.querySelector("#dark").addEventListener("click", dark_mode);
+
+    const savedMode = localStorage.getItem("purple_cello_trichrome_color_mode");
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (savedMode) {
+        document.querySelector("body").setAttribute("mode", savedMode);
+    } else if (isDarkMode) {
+        document.querySelector("body").setAttribute("mode", "dark");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", main);
